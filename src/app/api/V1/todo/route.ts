1162898317745b9343510cb2fb78220d2f4a2d5c
AniwatMeMix <aniwat.n@kkumail.com>
2024-09-30 23:03:22 +0700

    v1

---
+++ src/app/api/V1/todo/route.ts
@@ -0,0 +1,36 @@
+import { connectToDatabase } from "@/lib/mongodb";
+import Todo from "@/models/todo";
+import { NextRequest, NextResponse } from "next/server";
+
+//CRUD => 
+//READ data
+//url => api/v1/todo
+export async function GET() {
+  try {
+    await connectToDatabase();
+    const todoResult = await Todo.find({});
+    return NextResponse.json({ data: todoResult });
+  } catch (err) {
+    return NextResponse.json({
+      error: err,
+    });
+  }
+} 
+//Create new record
+//req => { name:"", note:""}
+//url => api/v1/todo
+export async function POST(req: NextRequest){
+  try{
+  const body = await req.json();
+  const res = await Todo.create(body);
+  return NextResponse.json({ data: res });
+  }catch(error){
+    return NextResponse.json({
+      error: error,
+    });
+  }
+ }
+//Update
+export async function PUT(){}
+//Delete
+export async function DELETE(){}
